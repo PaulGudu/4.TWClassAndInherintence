26c2cde471b641b5fe33e6fd7cdcd1ad79dc5d2e
Paul Gudu <3348225661@qq.com>
2017-06-12 21:04:28 +0800

    completed class inheritence test

--- main/person.js
+++ main/person.js
@@ -1,2 +1,19 @@
 // Write your code here
+class Person{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	getName(){
+		return this.name;
+	}
+
+	getAge(){
+		return this.age;
+	}
+	
+	 introduce() {
+			return "My name is "+ this.name + ". I am " + this.age +" years old."
+		}	
+}
 module.exports = Person
--- main/student.js
+++ main/student.js
@@ -1,3 +1,13 @@
 let Person = require('./person')
 // Write your code here
+class Student extends Person {
+	constructor(name, age, klass){
+		super(name, age);
+		this.klass = klass;
+	} 
+
+	 introduce() {
+		return super.introduce() +" I am a Student. I am at Class " + this.klass + "."; 
+	}
+}
 module.exports = Student
